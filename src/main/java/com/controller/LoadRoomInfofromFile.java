0cbb4e8f816e3376fed1c0cac92345e21ee2ef5e
Yoonjeong_Shin <yjshin124.gmail.com>
2024-04-03 20:02:55 +0900

    Feat : Room

--- src/main/java/com/controller/LoadRoomInfofromFile.java
+++ src/main/java/com/controller/LoadRoomInfofromFile.java
@@ -1,4 +1,18 @@
 package com.controller;
 
+import com.dto.Room;
+
+import java.io.*;
+import java.util.List;
+
 public class LoadRoomInfofromFile {
+    public List<Room> loadRoom() {
+        File source = new File("src/main/java/com/repository/RoomData");
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(source)))) {
+            return (List<Room>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/main/java/com/controller/SaveRoomInfotoFile.java
+++ src/main/java/com/controller/SaveRoomInfotoFile.java
@@ -1,4 +1,19 @@
 package com.controller;
 
+import com.dto.Room;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SaveRoomInfotoFile {
+    public void saveRoom(List<Room> rooms) {
+        File target = new File("src/main/java/com/repository/RoomData");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            oos.writeObject(rooms);
+            System.out.println("객실 정보가 성공적으로 저장되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
